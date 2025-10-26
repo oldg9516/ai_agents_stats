cdd83137ba0db6264fc5eb0089b476ad20a58606
Gleb Zavalov <gleb@levhaolam.com>
2025-10-27 00:01:27 +0300

    fix AppSidebar

--- app/detailed-stats/page.tsx
+++ app/detailed-stats/page.tsx
@@ -1,9 +1,9 @@
-import { Suspense } from 'react'
 import { AppSidebar } from '@/components/app-sidebar'
-import { SiteHeader } from '@/components/site-header'
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { DetailedStatsContent } from '@/components/detailed-stats-content'
 import { TableSkeleton } from '@/components/loading/table-skeleton'
+import { SiteHeader } from '@/components/site-header'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { Suspense } from 'react'
 
 /**
  * Detailed Stats Page - Full page view of detailed statistics table
@@ -23,11 +23,11 @@ export default function DetailedStatsPage() {
 				} as React.CSSProperties
 			}
 		>
-			<AppSidebar />
+			<AppSidebar variant='inset' />
 			<SidebarInset>
 				<SiteHeader />
-				<div className="flex flex-1 flex-col">
-					<div className="@container/main flex flex-1 flex-col gap-2">
+				<div className='flex flex-1 flex-col'>
+					<div className='@container/main flex flex-1 flex-col gap-2'>
 						<Suspense fallback={<TableSkeleton />}>
 							<DetailedStatsContent />
 						</Suspense>
